5fc6779b95096dbd6b751a23d89c29a681c38234
Imad Ali Khan Abbasi <imad.ali@locai.ai>
2025-07-20 05:09:04 +0500

    Update route.ts

--- src/app/api/latest-posts/route.ts
+++ src/app/api/latest-posts/route.ts
@@ -1,31 +1,102 @@
-// src/app/api/my-proxy/route.ts
-
-import { MediumHandler } from '@/lib/medium-util';
+import * as rssJson from 'rss-to-json';
+import * as cheerio from 'cheerio';
 import { NextResponse } from 'next/server';
+
+export interface Post {
+  id?: string;
+  title?: string;
+  description?: string;
+  link?: string;
+  autho?: string;
+  published?: number;
+  created?: number;
+  category?: Array<any>;
+  content?: string | any;
+  encolures?: any;
+  media?: any;
+
+  img?: string;
+  imgAlt?: string;
+  descriptionHtml?: string;
+}
+
+export interface Feed {
+  title: string;
+  description: string;
+  link: string;
+  image: string;
+  category: Array<string>;
+  items: Array<Post>;
+}
+
+class MediumUtils {
+  static __instance: MediumUtils;
+
+  constructor() {
+    MediumUtils.__instance = this;
+  }
+
+  static getInstance() {
+    return MediumUtils.__instance ?? new MediumUtils();
+  }
+
+  public async fetchLatestFromFeed(): Promise<Post[] | null> {
+        try {
+
+            const mediumUrl = process.env.MEDIUM_URL ?? 'https://imixtron.medium.com/feed';
+    
+            if (!mediumUrl) {
+                return null
+            }
+    
+            // getxmldata(mediumUrl);
+    
+            const res: Feed = await rssJson.parse(mediumUrl, { 
+                responseType: 'text',
+                headers: {
+                    Accept: 'application/xml',
+                    "User-Agent": 'imixtron-com'
+                }
+            });
+    
+            if (!res || !res?.items) {
+                return null;
+            }
+
+            return res?.items?.map((post) => {
+                const descAttrs = this.descriptionParser(post?.description);
+                return <Post>{ ...post, descriptionHtml: post?.description, img: descAttrs.imageSrc, imgAlt: descAttrs.imageAlt, description: descAttrs.description }
+            }).sort((a:any,b:any) => b?.created - a?.created).slice(0, 4);
+        } catch (e) {
+            console.log(e);
+            return [];
+        }
+  }
+
+  private descriptionParser(descriptionHtml: string | undefined) {
+
+    if (!descriptionHtml) {
+      return {};
+    }
+
+    const $ = cheerio.load(descriptionHtml);
+
+    const lastImg = $('.medium-feed-image img').last()
+    const imageSrc = lastImg.attr('src');
+    const imageAlt = lastImg.attr('alt')
+
+    let description = $('.medium-feed-snippet').text();
+    description = description?.replace(/[\n\t]/g, '').trim()
+
+    return { imageSrc, imageAlt, description };
+  }
+}
+
+const MediumHandler = MediumUtils.getInstance();
 
 export async function GET() {
   try {
     const posts = await MediumHandler.fetchLatestFromFeed();
-    console.log(posts);
-    // console.log(posts);
-    // const externalRes = await fetch('https://your-secret-api.com/data', {
-    //   method: 'GET',
-    //   headers: {
-    //     Authorization: `Bearer ${process.env.API_SECRET}`,
-    //     'Content-Type': 'application/json',
-    //   },
-    //   // Optional: Set cache to no-store if data is dynamic
-    //   cache: 'no-store',
-    // });
-
-    // if (!externalRes.ok) {
-    //   return NextResponse.json(
-    //     { error: 'Failed to fetch external API' },
-    //     { status: externalRes.status }
-    //   );
-    // }
-
-    // const data = await externalRes.json();
     return NextResponse.json(posts);
   } catch (err) {
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
